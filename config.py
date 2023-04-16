575ca6dffd999c3e887a5042059665d50cdc50bb
Ryan Dsilva <ryan.dsilva.98@gmail.com>
2023-04-16 11:06:36 -0500

    added deployment config

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 config = {
-  "HOST": "127.0.0.1",
-  "PORT": 5000,
-  "DEBUG": True,
+  "HOST": "0.0.0.0",
+  "PORT": int(os.environ.get('PORT', 5000)),
+  "DEBUG": False,
   "DB_HOST" : os.environ['MONGO_URL'],
   "JWT_KEY" : os.environ['JWT_KEY'],
   "TOKEN_EXPIRE_HOURS" : 12,
